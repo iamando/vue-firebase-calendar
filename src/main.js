aabd44c1016771da870f7135eba3e38a252f8242
ando-ramanamihanta <ramanamihanta.ando@gmail.com>
2021-04-13 13:54:28 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextAreaAutoSize from "vue-textarea-autosize";
+import firebase from "firebase";
+import "firebase/firestore";
 
-Vue.config.productionTip = false
+Vue.use(VueTextAreaAutoSize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  // Config
+});
+
+export const db = firebase.firestore();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
